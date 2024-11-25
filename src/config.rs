c8829a3576dea84e43e87217c0f70affd22dfe22
Julia Mertz <info@juliamertz.dev>
2024-11-26 00:39:40 +0100

    fix default values for cli arguments

--- src/config.rs
+++ src/config.rs
@@ -33,6 +33,7 @@ fn styles() -> Styles {
 #[command(author, version, about, long_about = None, styles = styles())]
 /// Theme generator for Rosé Pine
 pub struct Args {
+    /// path to directory where generated files will be output
     #[clap(long, short, default_value = "dist")]
     pub out: PathBuf,
 
@@ -42,37 +43,31 @@ pub struct Args {
     pub tera: bool,
 
     #[clap(long, short)]
+    /// recursively generate templates in source directory
+    pub recurse: bool,
+
+    #[clap(long, short, default_value = "hex")]
     /// default color formatting
-    pub format: Option<Format>,
+    pub format: Format,
 
-    #[clap(long, short)]
+    #[clap(long, short, default_value = "parenthesis")]
     /// bracket type for role groups
-    pub delimiter: Option<Delimiter>,
+    pub delimiter: Delimiter,
 
-    #[clap(long, short)]
+    #[clap(long, short, default_value = "|")]
     /// charachter to use as seperator in role groups
-    pub seperator: Option<char>,
+    pub seperator: char,
 
     #[clap(long)]
     /// always add alpha values
     pub force_alpha: bool,
 
-    #[clap(long, short)]
+    #[clap(long, short, default_value = "$")]
     /// variable prefix
-    pub prefix: Option<char>,
+    pub prefix: char,
 
     /// path to template file
     pub template_source: PathBuf,
-}
-
-impl Args {
-    pub fn no_options(&self) -> bool {
-        self.format.is_none()
-            && self.delimiter.is_none()
-            && self.seperator.is_none()
-            && self.prefix.is_none()
-            && !self.force_alpha
-    }
 }
 
 impl From<&Args> for Config {
@@ -81,12 +76,12 @@ impl From<&Args> for Config {
             #[cfg(feature = "templating")]
             tera: value.tera,
             parse: parse::ParseOptions {
-                prefix: value.prefix.unwrap_or('$'),
-                seperator: value.seperator.unwrap_or('|'),
-                delimiter: value.delimiter.unwrap_or(Delimiter::Parenthesis),
+                prefix: value.prefix,
+                seperator: value.seperator,
+                delimiter: value.delimiter,
             },
             generate: generate::Options {
-                format: value.format.unwrap_or(Format::Hex),
+                format: value.format,
                 strip_spaces: false,
                 force_alpha: value.force_alpha,
             },
