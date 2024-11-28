0d7ff572545ea093c0172f3da21209a9bfac80bf
Julia Mertz <info@juliamertz.dev>
2024-11-28 02:41:17 +0100

    refactor color palette

--- palette/variant.rs
+++ palette/variant.rs
@@ -19,7 +19,116 @@ pub enum VariantKind {
     Dark,
 }
 
+type Colors = (Rgb, Hsl);
+
+pub struct Palette {
+    base: Colors,
+    surface: Colors,
+    overlay: Colors,
+    muted: Colors,
+    subtle: Colors,
+    text: Colors,
+    love: Colors,
+    gold: Colors,
+    rose: Colors,
+    pine: Colors,
+    foam: Colors,
+    iris: Colors,
+    highlight_low: Colors,
+    highlight_med: Colors,
+    highlight_high: Colors,
+}
+
+impl Palette {
+    pub const MAIN: Palette = Palette {
+        base: (rgb(25, 23, 36), hsl(249, 22, 12)),
+        surface: (rgb(31, 29, 46), hsl(247, 23, 15)),
+        overlay: (rgb(38, 35, 58), hsl(248, 25, 18)),
+        muted: (rgb(110, 106, 134), hsl(249, 12, 47)),
+        subtle: (rgb(144, 140, 170), hsl(248, 15, 61)),
+        text: (rgb(224, 222, 244), hsl(245, 50, 91)),
+        love: (rgb(235, 111, 146), hsl(343, 76, 68)),
+        gold: (rgb(246, 193, 119), hsl(35, 88, 72)),
+        rose: (rgb(235, 188, 186), hsl(2, 55, 83)),
+        pine: (rgb(49, 116, 143), hsl(197, 49, 38)),
+        foam: (rgb(156, 207, 216), hsl(189, 43, 73)),
+        iris: (rgb(196, 167, 231), hsl(267, 57, 78)),
+        highlight_low: (rgb(33, 32, 46), hsl(244, 18, 15)),
+        highlight_med: (rgb(64, 61, 82), hsl(249, 15, 28)),
+        highlight_high: (rgb(82, 79, 103), hsl(248, 13, 36)),
+    };
+    pub const MOON: Palette = Palette {
+        base: (rgb(35, 33, 54), hsl(246, 24, 17)),
+        surface: (rgb(42, 39, 63), hsl(248, 24, 20)),
+        overlay: (rgb(57, 53, 82), hsl(248, 21, 26)),
+        muted: (rgb(110,106,134), hsl(249,12,47)),
+        subtle: (rgb(144,140,170), hsl(248,15,61)),
+        text: (rgb(224,222,244), hsl(245,50,91)),
+        love: (rgb(235,111,146), hsl(343,76,68)),
+        gold: (rgb(246,193,119), hsl(35,88,72)),
+        rose: (rgb(234,154,151), hsl(2,66,75)),
+        pine: (rgb(62,143,176), hsl(197,48,47)),
+        foam: (rgb(156,207,216), hsl(189,43,73)),
+        iris: (rgb(196,167,231), hsl(267,57,78)),
+        highlight_low: (rgb(42, 40, 62), hsl(245, 22, 20)),
+        highlight_med: (rgb(68, 65, 90), hsl(247, 16, 30)),
+        highlight_high: (rgb(86, 82, 110), hsl(249, 15, 38)),
+    };
+    pub const DAWN: Palette = Palette {
+        base: (rgb(250, 244, 237), hsl(32, 57, 95)),
+        surface: (rgb(255, 250, 243), hsl(35, 100, 98)),
+        overlay: (rgb(242, 233, 222), hsl(33, 43, 91)),
+        muted: (rgb(152, 147, 165), hsl(257, 9, 61)),
+        subtle: (rgb(121, 117, 147), hsl(248, 12, 52)),
+        text: (rgb(87, 82, 121), hsl(248, 19, 40)),
+        love: (rgb(180, 99, 122), hsl(343, 35, 55)),
+        gold: (rgb(234, 157, 52), hsl(35, 81, 56)),
+        rose: (rgb(215, 130, 126), hsl(3, 53, 67)),
+        pine: (rgb(40, 105, 131), hsl(197, 53, 34)),
+        foam: (rgb(86, 148, 159), hsl(189, 30, 48)),
+        iris: (rgb(144, 122, 169), hsl(268, 21, 57)),
+        highlight_low: (rgb(244, 237, 232), hsl(25, 35, 93)),
+        highlight_med: (rgb(223, 218, 217), hsl(10, 9, 86)),
+        highlight_high: (rgb(206, 202, 205), hsl(315, 4, 80)),
+    };
+
+    pub const fn get_role(&self, role: &Role) -> Colors {
+        match role {
+            Role::Base => self.base,
+            Role::Surface => self.surface,
+            Role::Overlay => self.overlay,
+            Role::Muted => self.muted,
+            Role::Subtle => self.subtle,
+            Role::Text => self.text,
+            Role::Love => self.love,
+            Role::Gold => self.gold,
+            Role::Rose => self.rose,
+            Role::Pine => self.pine,
+            Role::Foam => self.foam,
+            Role::Iris => self.iris,
+            Role::HighlightLow => self.highlight_low,
+            Role::HighlightMed => self.highlight_med,
+            Role::HighlightHigh => self.highlight_high,
+        }
+    }
+
+    pub const fn get_rgb(&self, role: &Role) -> Rgb {
+        self.get_role(role).0
+    }
+    pub const fn get_hsl(&self, role: &Role) -> Hsl {
+        self.get_role(role).1
+    }
+}
+
 impl Variant {
+    pub const fn get_palette(&self) -> Palette {
+        match self {
+            Variant::Main => Palette::MAIN,
+            Variant::Moon => Palette::MOON,
+            Variant::Dawn => Palette::DAWN,
+        }
+    }
+
     pub fn id(&self) -> String {
         match self {
             Self::Main => "rose-pine",
@@ -61,75 +170,18 @@ impl Variant {
             .collect()
     }
 
-    pub fn get_rgb(&self, role: Role) -> Rgb {
-        match self {
-            Variant::Main => match role {
-                Role::Base => rgb(25, 23, 36),
-                Role::Surface => rgb(31, 29, 46),
-                Role::Overlay => rgb(38, 35, 58),
-                Role::Muted => rgb(110, 106, 134),
-                Role::Subtle => rgb(144, 140, 170),
-                Role::Text => rgb(224, 222, 244),
-                Role::Love => rgb(235, 111, 146),
-                Role::Gold => rgb(246, 193, 119),
-                Role::Rose => rgb(235, 188, 186),
-                Role::Pine => rgb(49, 116, 143),
-                Role::Foam => rgb(156, 207, 216),
-                Role::Iris => rgb(196, 167, 231),
-                Role::HighlightLow => rgb(33, 32, 46),
-                Role::HighlightMed => rgb(64, 61, 82),
-                Role::HighlightHigh => rgb(82, 79, 103),
-            },
-            Variant::Moon => match role {
-                Role::Base => rgb(35, 33, 54),
-                Role::Surface => rgb(42, 39, 63),
-                Role::Overlay => rgb(57, 53, 82),
-                Role::Muted => rgb(110, 106, 134),
-                Role::Subtle => rgb(144, 140, 170),
-                Role::Text => rgb(224, 222, 244),
-                Role::Love => rgb(235, 111, 146),
-                Role::Gold => rgb(246, 193, 119),
-                Role::Rose => rgb(234, 154, 151),
-                Role::Pine => rgb(62, 143, 176),
-                Role::Foam => rgb(156, 207, 216),
-                Role::Iris => rgb(196, 167, 231),
-                Role::HighlightLow => rgb(42, 40, 62),
-                Role::HighlightMed => rgb(68, 65, 90),
-                Role::HighlightHigh => rgb(86, 82, 110),
-            },
-            Variant::Dawn => match role {
-                Role::Base => rgb(250, 244, 237),
-                Role::Surface => rgb(255, 250, 243),
-                Role::Overlay => rgb(242, 233, 222),
-                Role::Muted => rgb(152, 147, 165),
-                Role::Subtle => rgb(121, 117, 147),
-                Role::Text => rgb(87, 82, 121),
-                Role::Love => rgb(180, 99, 122),
-                Role::Gold => rgb(234, 157, 52),
-                Role::Rose => rgb(215, 130, 126),
-                Role::Pine => rgb(40, 105, 131),
-                Role::Foam => rgb(86, 148, 159),
-                Role::Iris => rgb(144, 122, 169),
-                Role::HighlightLow => rgb(244, 237, 232),
-                Role::HighlightMed => rgb(223, 218, 217),
-                Role::HighlightHigh => rgb(206, 202, 205),
-            },
-        }
+    pub const fn get_rgb(&self, role: Role) -> Rgb {
+        self.get_palette().get_rgb(&role)
     }
 
-    pub fn get_hsl(&self, role: Role) -> Hsl {
-        use colors_transform::{Color, Rgb}; // TODO: Drop this lib?
-
-        let (r, g, b) = self.get_rgb(role).into();
-        let hsl = Rgb::from(r as f32, g as f32, b as f32).to_hsl();
-        Hsl::new(
-            hsl.get_hue().round() as u16,
-            hsl.get_saturation().round() as u8,
-            hsl.get_lightness().round() as u8,
-        )
+    pub const fn get_hsl(&self, role: Role) -> Hsl {
+        self.get_palette().get_hsl(&role)
     }
 }
 
-fn rgb(r: u8, g: u8, b: u8) -> Rgb {
-    Rgb::new(r, g, b)
+const fn rgb(r: u8, g: u8, b: u8) -> Rgb {
+    Rgb { r, g, b }
+}
+const fn hsl(h: u16, s: u8, l: u8) -> Hsl {
+    Hsl { h, s, l }
 }
