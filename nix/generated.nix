27df71f2305bd63f5558a3b7d274ac0b28eb4385
juliamertz <35079666+juliamertz@users.noreply.github.com>
2026-06-15 18:48:45 +0000

    [Update artifacts] fix: cd workflow

--- nix/generated.nix
+++ nix/generated.nix
@@ -1,23 +1,23 @@
 { fetchurl, ... }:
 let
   repo = "juliamertz/rose-pine-build";
-  tag = "v0.1.0";
+  tag = "v0.2.0";
 in
 {
   x86_64-linux = fetchurl {
     url = "https://github.com/${repo}/releases/download/${tag}/rose-pine-build-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = "12rvl31lx3dllnm9r8arw7hm993yk5sb0ahbzgw2d968gqmhvwc7";
+    sha256 = "14ab35hv29kaw8pih0fvf5xhizaw5nw55d792v6vzqijhz31bbxb";
   };
   aarch64-linux = fetchurl {
     url = "https://github.com/${repo}/releases/download/${tag}/rose-pine-build-aarch64-unknown-linux-gnu.tar.gz";
-    sha256 = "0jpj5rkbixk96g0b754pvqk1s2zqpyhvh9wbkm67qni41k0wpnv2";
+    sha256 = "19in6v78qald5wd61j8iksggbkhb9kdas70vf604rqlk0c4dqg4c";
   };
   x86_64-darwin = fetchurl {
     url = "https://github.com/${repo}/releases/download/${tag}/rose-pine-build-x86_64-apple-darwin.tar.gz";
-    sha256 = "1sdx0i10zqysmsgcsgdyblpfrnh1vwxwxc7vqmaqqm7ch8pw5z94";
+    sha256 = "0cac6dc3sj87ydzx92j0l4jyy7jv5x0fvan2vv70aks3wldrimc6";
   };
   aarch64-darwin = fetchurl {
     url = "https://github.com/${repo}/releases/download/${tag}/rose-pine-build-aarch64-apple-darwin.tar.gz";
-    sha256 = "19lxgzwswy65dl79jvp4lhpjbgjfvj93vb7skfrgh2iih6rirykk";
+    sha256 = "14zx92wdzicm37mp75iyz64pwfs4z4dx091dr3mn7iwrh2177xbl";
   };
 }
